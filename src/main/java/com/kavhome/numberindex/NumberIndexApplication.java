0275e0f7202a645c4f62844f6979743a9f831e2b
Kolyuchkin Aleksandr <koljuchkin.aleksandr@alphaopen.com>
2020-10-29 00:10:05 +0300

    First commit with service template

---
+++ src/main/java/com/kavhome/numberindex/NumberIndexApplication.java
@@ -0,0 +1,13 @@
+package com.kavhome.numberindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kavhome/numberindex/NumberIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavhome.numberindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
